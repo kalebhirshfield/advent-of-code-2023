7fee56370296d85529466a151c6de903b3e96f83
Kaleb Hirshfield <kaleb.hirshfield@protonmail.com>
2023-12-05 13:17:23 +0000

    initial commit

---
+++ day1/part1.py
@@ -0,0 +1,20 @@
+def main():
+    file = open("input", "r")
+    lines = file.readlines()
+    total = 0
+
+    for line in lines:
+        digits = ""
+        for letter in line:
+            if letter.isnumeric():
+                digits += letter
+        digit1 = digits[0]
+        digit2 = digits[-1]
+        num = int(digit1 + digit2)
+        total += num
+
+    return total
+
+
+if __name__ == "__main__":
+    print(main())
